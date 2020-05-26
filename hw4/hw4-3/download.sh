fa655c55db466c5b2224b97cf359110fa275facf
YJC <openopentw@gmail.com>
2020-05-27 02:33:29 +0800

    Add reproduce codes for hw4-3.

---
+++ hw4/hw4-3/download.sh
@@ -0,0 +1,3 @@
+downloaded_model=$1
+
+wget https://www.dropbox.com/s/id1hbrydup059ey/hw4-3.bin?dl=1 -O ${downloaded_model}
---
+++ hw4/hw4-3/reproduce.sh
@@ -0,0 +1,21 @@
+downloaded_model=$1
+test_path=$2
+output=$3
+
+mv ${downloaded_model} ./model/pytorch_model.bin
+
+python ./examples/run_squad.py \
+	--model_type bert \
+	--do_eval \
+	--model_name_or_path ./model \
+	--tokenizer_name bert-base-chinese \
+	--train_file Data/hw4-3_train.json \
+	--predict_file ${test_path} \
+	--max_seq_length 384 \
+	--per_gpu_eval_batch_size 100 \
+	--output_dir ./model \
+	--fp16
+
+python3 ./process_ans.py \
+	./model/predictions_.json \
+	${output}
